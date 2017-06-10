013e869c8c0a1072a7a3341a4dd46425f4f39ff4
Ahmed Moneeb <ahmedhamdy395@gmail.com>
2017-06-10 21:44:48 +0200

    changed the server IP and database name

--- backend/connectDB.php
+++ backend/connectDB.php
@@ -2,12 +2,15 @@
 $servername = "localhost";
 $username = "root";
 $password = "root";
-$mysql_database = "CloudSoundSystem";
+$mysql_database = "airwave";
 // Create connection
 $conn =mysqli_connect($servername, $username, $password, $mysql_database);
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-
+else
+{
+	//echo "success";
+}
 ?> 
--- backend/connectMQTT.php
+++ backend/connectMQTT.php
@@ -1,4 +1,4 @@
  <?php
-$servername = "localhost";
+$servername = "54.157.9.100";
 $mqtt = new phpMQTT($servername, 1883, "phpMQTT Pub Example");
 ?> 
